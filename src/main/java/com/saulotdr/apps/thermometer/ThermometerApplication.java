110dd332add1cf0f233e82f4baf93475513a944d
Saulo Ferreira <sauloferreira@nearsure.net>
2022-03-19 14:39:17 -0300

    Initial commit

---
+++ src/main/java/com/saulotdr/apps/thermometer/ThermometerApplication.java
@@ -0,0 +1,13 @@
+package com.saulotdr.apps.thermometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saulotdr/apps/thermometer/ThermometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saulotdr.apps.thermometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThermometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
